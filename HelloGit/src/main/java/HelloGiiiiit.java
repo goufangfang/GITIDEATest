92c465c59884ba612ae73b3cea1101ab53585097
goufangfang <Lmk139388>
2020-07-30 14:24:26 +0800

    test commit

---
+++ HelloGit/src/main/java/HelloGiiiiit.java
@@ -0,0 +1,9 @@
+/**
+ * @author mengkunli
+ * @date 2020/7/30 - 2:20 下午
+ */
+public class HelloGiiiiit {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
